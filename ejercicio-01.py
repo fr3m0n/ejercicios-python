6344d83003219df0be0929b77ffeee4bba7f35b3
fchau <fchau@hotmail.es>
2021-11-03 13:59:08 +0100

    Cambio en el archivo

--- ejercicio-01.py
+++ ejercicio-01.py
@@ -1,6 +1,11 @@
-nl=[]
+#Agregamos una lista vacia
+lista=[]
+#Recorremos el rango
 for x in range(2000, 3200):
+#Comprobamos si es multiplo de 7 y de 5
     if (x%7==0) and (x%5==0):
-        nl.append(str(x))
-print (','.join(nl))
+#Agregamos a la lista y lo convertimos a un string
+        lista.append(str(x))
+#Visualizamos por pantalla la lista
+print (','.join(lista))
 
