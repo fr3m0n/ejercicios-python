1f9e2fcdf6584241a04445678a08ef6ccc5ce211
Freddy Chau Escalante <45338607+fr3m0n@users.noreply.github.com>
2021-11-03 14:02:03 +0100

    Update ejercicio-01.py

--- ejercicio-01.py
+++ ejercicio-01.py
@@ -6,6 +6,6 @@ for x in range(2000, 3200):
     if (x%7==0) and (x%5==0):
 #Agregamos a la lista y lo convertimos a un string
         lista.append(str(x))
-#Visualizamos por pantalla la lista
+#Visualizamos por pantalla la lista separadas por comas
 print (','.join(lista))
 
